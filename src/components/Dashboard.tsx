d064555278aad2e25ac80da72242ffb8da6967f3
Gustavo <gustavoalzarate@gmail.com>
2025-02-23 10:47:38 -0600

    fix: validate roleId

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,17 +1,21 @@
 import { useAppSelector } from "../store/store"
+import Jobs from "./DashboardEmplooye/Jobs"
 import Main from "./DashboardRecruiter/Main"
 
 export default function Dashboard() {
-    const { roleId } = useAppSelector((state) => state.userSlice)
-    return (
-        <>
-            <div className="w-full pt-5">
-                {roleId === 1 ? (
-                    <Main />
-                ) : (
-                    <></>
-                )}
-            </div>
-        </>
-    )
+  const { roleId } = useAppSelector((state) => state.userSlice)
+  if(roleId <= 0) return <></>
+  return (
+    <>
+      <div className="w-full pt-5">
+        {roleId === 1 ? (
+          <Main />
+        ) : (
+          <>
+            <Jobs />
+          </>
+        )}
+      </div>
+    </>
+  )
 }
